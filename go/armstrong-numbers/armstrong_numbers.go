56ab8caff50986a71cc8d322cc161656945bc92e
cunbex <hamzalagab.tech@gmail.com>
2022-11-13 19:19:48 +0100

    Following exercises solution added: Armstrong Numbers

--- go/armstrong-numbers/armstrong_numbers.go
+++ go/armstrong-numbers/armstrong_numbers.go
@@ -1,5 +1,17 @@
+// BenchmarkIsNumber-2       377844              3139 ns/op             162 B/op         38 allocs/op
 package armstrong
 
+import (
+	"math"
+	"strconv"
+)
+
 func IsNumber(n int) bool {
-	panic("Please implement the IsNumber function")
+	numb := strconv.Itoa(n)
+	var result int
+	for _, v := range numb {
+		temp, _ := strconv.ParseFloat(string(v), 64)
+		result += int(math.Pow(temp, float64(len(numb))))
+	}
+	return result == n
 }
