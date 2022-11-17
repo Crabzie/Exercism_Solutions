eb93d2306ac2cff485f072a4c435e1c5568f0757
cunbex <hamzalagab.tech@gmail.com>
2022-11-17 19:00:23 +0100

    Following exercises solved: Collatz Conjecture.

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,19 @@
 package collatzconjecture
 
+import "errors"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	var result int
+	if n <= 0 {
+		return 0, errors.New("wrong input")
+	}
+	for n > 1 {
+		if n%2 == 0 {
+			n /= 2
+		} else {
+			n = 3*n + 1
+		}
+		result++
+	}
+	return result, nil
 }
