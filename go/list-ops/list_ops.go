0252f8ae44b7efe06172609a9a95fe6bf23fee5e
cunbex <hamzalagab.tech@gmail.com>
2022-11-21 14:52:00 +0100

    Following exercises solution added: List Opts

--- go/list-ops/list_ops.go
+++ go/list-ops/list_ops.go
@@ -4,33 +4,86 @@ package listops
 type IntList []int
 
 func (s IntList) Foldl(fn func(int, int) int, initial int) int {
-	panic("Please implement the Foldl function")
+	if s.Length() != 0 {
+		result := initial
+		for _, v := range s {
+			result = fn(result, v)
+		}
+		return result
+	}
+	return initial
 }
 
 func (s IntList) Foldr(fn func(int, int) int, initial int) int {
-	panic("Please implement the Foldr function")
+	if s.Length() != 0 {
+		result := initial
+		for i := s.Length() - 1; i >= 0; i-- {
+			result = fn(s[i], result)
+		}
+		return result
+	}
+	return initial
 }
 
 func (s IntList) Filter(fn func(int) bool) IntList {
-	panic("Please implement the Filter function")
+	if s.Length() != 0 {
+		result := make(IntList, s.Length())
+		var i int
+		for _, v := range s {
+			if fn(v) {
+				result[i] = v
+				i++
+			}
+		}
+		return result[:i]
+	}
+	return []int{}
 }
-
 func (s IntList) Length() int {
-	panic("Please implement the Length function")
+	var result int
+	for range s {
+		result++
+	}
+	return result
 }
 
 func (s IntList) Map(fn func(int) int) IntList {
-	panic("Please implement the Map function")
+	result := make(IntList, s.Length())
+	if s.Length() != 0 {
+		for i, v := range s {
+			result[i] = fn(v)
+		}
+	}
+	return result
 }
 
 func (s IntList) Reverse() IntList {
-	panic("Please implement the Reverse function")
+	result := make(IntList, s.Length())
+	for i, j := 0, s.Length()-1; i < s.Length(); i, j = i+1, j-1 {
+		result[i] = s[j]
+	}
+	return result
 }
 
 func (s IntList) Append(lst IntList) IntList {
-	panic("Please implement the Append function")
+	result := make(IntList, s.Length()+lst.Length())
+	if s.Length() == 0 {
+		copy(result, lst)
+		return result
+	}
+	copy(result, s)
+	for i, j := s.Length(), 0; i < len(result); i, j = i+1, j+1 {
+		result[i] = lst[j]
+	}
+	return result
 }
-
 func (s IntList) Concat(lists []IntList) IntList {
-	panic("Please implement the Concat function")
+	var result IntList
+	result = result.Append(s)
+	for _, v := range lists {
+		if v.Length() != 0 {
+			result = result.Append(v)
+		}
+	}
+	return result
 }
