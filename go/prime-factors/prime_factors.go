b17f86088f3ceab05e5006ddfb2ce54f79a6ac21
cunbex <hamzalagab.tech@gmail.com>
2022-12-05 14:05:34 +0100

    Following exercise solution added: Prime Factors

--- go/prime-factors/prime_factors.go
+++ go/prime-factors/prime_factors.go
@@ -1,5 +1,15 @@
 package prime
 
-func Factors(n int64) []int64 {
-	panic("Please implement the Factors function")
+func numbers(n int64, result *[]int64) int64 {
+	for i := 2; i <= int(n) && n >= 1; i++ {
+		if n%int64(i) == 0 {
+			*result = append(*result, int64(i))
+			n = numbers(n/int64(i), result)
+		}
+	}
+	return 0
+}
+func Factors(n int64) (result []int64) {
+	numbers(n, &result)
+	return
 }
