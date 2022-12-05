1d01c727252b8e1fa77ae366c67b67ac824b4493
cunbex <hamzalagab.tech@gmail.com>
2022-12-05 13:28:29 +0100

    Following exercises solution added: Nth Prime

--- go/nth-prime/nth_prime.go
+++ go/nth-prime/nth_prime.go
@@ -1,6 +1,32 @@
 package prime
 
+import "errors"
+
 // Nth returns the nth prime number. An error must be returned if the nth prime number can't be calculated ('n' is equal or less than zero)
+func checkIfPrime(n int) bool {
+	for j := 2; j < n/2; j++ {
+		if n%j == 0 {
+			return false
+		}
+	}
+	return true
+}
 func Nth(n int) (int, error) {
-	panic("Please implement the Nth function")
+	switch n {
+	case 0:
+		return 0, errors.New("there is no zeroth prime")
+	case 1:
+		return 2, nil
+	case 2:
+		return 3, nil
+	default:
+		var result int
+		for result = 4; n > 1; result++ {
+			ok := checkIfPrime(result)
+			if ok {
+				n--
+			}
+		}
+		return result - 1, nil
+	}
 }
