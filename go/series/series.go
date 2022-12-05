f6634ef7ffce7f9bd50b1471f1a095e047d6c266
cunbex <hamzalagab.tech@gmail.com>
2022-12-05 15:32:09 +0100

    Following exercises solution added: Series

--- go/series/series.go
+++ go/series/series.go
@@ -1,9 +1,23 @@
 package series
 
-func All(n int, s string) []string {
-	panic("Please implement the All function")
+func All(n int, s string) (result []string) {
+	for i := range s {
+		if i+n > len(s) {
+			break
+		}
+		result = append(result, s[i:i+n])
+	}
+	return
 }
 
 func UnsafeFirst(n int, s string) string {
-	panic("Please implement the UnsafeFirst function")
+	return All(n, s)[0]
+}
+
+/* bonus exercise */
+func First(n int, s string) (first string, ok bool) {
+	if n > len(s) {
+		return
+	}
+	return UnsafeFirst(n, s), true
 }
